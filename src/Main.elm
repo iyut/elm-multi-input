66a540dc09338b8e02175dff6fdfa5720256eb07
Luthfi <iyut85@yahoo.com>
2021-12-02 21:54:09 +0700

    add styling to input

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,12 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onClick, onInput)
+import Css exposing (..)
+import Html
+
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (..)
+import Html.Styled.Events exposing ( onClick, onInput )
 
 
 -- MAIN
@@ -12,7 +15,7 @@ main = Browser.element
          { init = init
          , update = update
          , subscriptions = subscriptions
-         , view = view 
+         , view = view >> toUnstyled
          }
 
 
@@ -111,27 +114,48 @@ view model =
   div [] 
   [ text "Accounts"
   , viewRows model.accounts 
-  , button [ onClick AddRow ] [ text "-" ]
+  , button [ onClick AddRow ] [ text "+" ]
   ]
   
 viewRows : List Account -> Html Msg
 viewRows acts = 
-  div [] ( List.map viewAccount acts )
+  div [] ( List.map viewRowInputs acts )
 
 
-viewAccount : Account -> Html Msg 
-viewAccount act = 
-  div [] 
-  [ viewInput "text" "ID" (String.fromInt act.id) NoOp
-  , viewInput "text" "Name" act.name NoOp
-  , viewInput "text" "Age" (String.fromInt act.age) NoOp
-  , button [ onClick ( RemoveRow act.id ) ] [ text "+" ]
-  ]
+viewRowInputs : Account -> Html Msg 
+viewRowInputs act = 
+  div 
+    [ class "vc_row"
+    , css
+        [ marginBottom (px 5) ]
+    ] 
+    [ viewInput "text" "ID" (String.fromInt act.id) NoOp
+    , viewInput "text" "Name" act.name NoOp
+    , viewInput "text" "Age" (String.fromInt act.age) NoOp
+    , button [ onClick ( RemoveRow act.id ) ] [ text "-" ]
+    ]
 
 viewInput : String -> String -> String -> (String -> msg) -> Html msg
 viewInput typ plcholder val evt = 
-  label []
-  [ text plcholder
-  , input [ type_ typ, placeholder plcholder, value val, onInput evt ] []
-  ]
+  label 
+    [ class "vc_row_label"
+    , attribute "data-label" ( "label_" ++ typ )
+    , css
+        [ display inlineBlock
+        , padding (px 5)
+        ]
+    ]
+    [ input 
+      [ type_ typ
+      , placeholder plcholder
+      , value val
+      , class "vc_row_input" 
+      , onInput evt
+      , css 
+          [ padding (px 5)
+          , border3 (px 1) solid (rgba 0 0 0 0.2)
+          , borderRadius (px 4)
+          ]
+      ] []
+    ]
   
